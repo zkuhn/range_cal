5ff10d751641a2a4b945f195b10adbfe7730e405
zakuhn <zakuhn@fusionent.com>
2019-10-14 01:53:48 -0700

    simplify main a bit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from linear_regression import linear_fit
 import matplotlib.pyplot as plt
 import numpy as np
+import ui
+import calib_workflow_ctrlr as cw
+import test_calib_workflow_ctrlr as tcw
+
+ 
 
 import mock_data as md
 
@@ -24,13 +29,6 @@ def simple_plot():
     plot_fit(points, m, b)
 
 
-def main():
-    """
-    Run our two sample data sets to start
-    """
-    simple_plot()
-    mock_data_plot()
-    
 def mock_data_plot():
     """
     Do a plot of the mock data supplied in the question
@@ -40,31 +38,19 @@ def mock_data_plot():
     # print(data_array)
     m,b = linear_fit(data_array)
     plot_fit(data_array, m, b)
-        
 
-def plot_fit(points, slope, y_intercept):
-    """
-    Plot the points and the linear fit line.
-    Label the plot so it's easy to see what data is plotted
-    """
 
-    fig, ax = plt.subplots()
-    
-    for x,y in points:
-    
-        ax.scatter(x,y)
-    
-    x_range = [a[0] for a in points]
-    x_range.sort()
-    
-    t = np.arange(x_range[0], x_range[-1])    
-    plt.plot(t, slope * t + y_intercept)
-    
-    ax.set_title("Slope = {:4.4f}".format(slope) + "  :  Y_intercept = {:4.2f}".format(y_intercept), fontsize=15)
-    ax.set_xlabel("Temperature" , fontsize=15)
-    ax.set_ylabel("Range", fontsize=15)
-       
-    plt.show( )
+def main():
+    """
+    Run our two sample data sets to start
+    """
+    # simple_plot()
+    # mock_data_plot()
+
+    fc = tcw.MockFixtureController()
+    tui = ui.TextUI()
+    tc = cw.CalibrationWorkflowController(fc, tui)
+    tc.run()
     
 
 if __name__ == "__main__":
